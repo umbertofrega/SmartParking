89da82deb2468cecb8159f83610dc2e13b6b618f
umbertofrega <umbertofrega@gmail.com>
2026-03-01 20:44:08 +0100

    added map functionalities

--- app/src/main/java/com/piattaforme/smartparking/MapActivity.kt
+++ app/src/main/java/com/piattaforme/smartparking/MapActivity.kt
@@ -2,6 +2,9 @@ package com.piattaforme.smartparking
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -10,27 +13,61 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), LocationListener {
     val locationPermissionCode = 100
+    private lateinit var mapView: MapView
+    private lateinit var  locationManager : LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Configuration.getInstance().load(applicationContext, applicationContext.getSharedPreferences("osmdroid_prefs", MODE_PRIVATE))
+        Configuration.getInstance().userAgentValue = applicationContext.packageName
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_map)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        mapView = findViewById(R.id.osmdroid_map)
+        mapView.setMultiTouchControls(true)
+        mapView.controller.setZoom(18.0)
+
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
         val requestLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
 
-        if(requestLocationPermission != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),locationPermissionCode)
+        if (requestLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                locationPermissionCode
+            )
         } else {
-            //TODO Show Map Position
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000L, 10f, this)
         }
+    }
+
+    override fun onLocationChanged(location: Location){
+        val geoPoint = GeoPoint(location.latitude,location.longitude)
+
+        mapView.overlays.clear()
+
+        val marker = Marker(mapView)
+        marker.position = geoPoint
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        mapView.overlays.add(marker)
+
+        mapView.controller.setCenter(geoPoint)
+        mapView.controller.zoomIn()
 
     }
 
@@ -43,9 +80,9 @@ class MapActivity : AppCompatActivity() {
 
         if(requestCode == locationPermissionCode){
             if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(this,"Accesso Autorizzato", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"Access Authorized", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this,"Accesso Negato", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"Access Refused", Toast.LENGTH_SHORT).show()
             }
         }
     }
