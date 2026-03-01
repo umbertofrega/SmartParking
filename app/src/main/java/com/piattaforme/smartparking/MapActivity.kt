228cecf62a2fe7eccf689d4d4a0938c588feb404
umbertofrega <umbertofrega@gmail.com>
2026-03-01 19:46:07 +0100

    added permissions request

--- app/src/main/java/com/piattaforme/smartparking/MapActivity.kt
+++ app/src/main/java/com/piattaforme/smartparking/MapActivity.kt
@@ -1,12 +1,19 @@
 package com.piattaforme.smartparking
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MapActivity : AppCompatActivity() {
+    val locationPermissionCode = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,31 @@ class MapActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val requestLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if(requestLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),locationPermissionCode)
+        } else {
+            //TODO Show Map Position
+        }
+
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == locationPermissionCode){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Accesso Autorizzato", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this,"Accesso Negato", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
