1e57694737b542f664adbc5306e70010ffc4379e
umbertofrega <umbertofrega@gmail.com>
2026-03-01 19:46:24 +0100

    added dependency in build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.osmdroid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
